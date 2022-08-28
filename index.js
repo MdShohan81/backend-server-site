022f9bc80c804691df2b39c057c265d125f0909a
MdShohan81 <mdshohan6563@gmail.com>
2022-08-28 13:51:08 +0600

    find all service data and dynamic service single data then update data

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+
+// middleware 
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or00c5l.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('carspot_project').collection('service');
+
+        app.get('/service', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+
+        app.get('/service/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        //PUT
+        app.put('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true};
+            const updatedDoc = {
+                $set: {
+                    ...updatedItem,
+                }
+            };
+            const result = await serviceCollection.updateOne(filter,updatedDoc,options);
+            res.send(result);
+        })
+            
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello carspot')
+})
+
+app.listen(port, () => {
+    console.log(`carspot app listeing ${port}`)
+})
