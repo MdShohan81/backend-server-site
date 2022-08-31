eacd62e86ed737144a28fbc775f8dd65d6681714
MdShohan81 <mdshohan6563@gmail.com>
2022-08-31 12:32:35 +0600

    delete comment

--- index.js
+++ index.js
@@ -13,22 +13,7 @@ app.use(cors())
 app.use(express.json());
 
 
-// function verifyJWT(req, res, next){
-//     const authHeader = req.headers.authorization;
-//     if(!authHeader){
-//         return res.status(401).send({message: 'unauthorized access'})
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if(err){
-//             return res.status(403).send({message: 'Forbidden access'})
-//         }
-//         console.log('decoded', decoded);
-//         req.decoded =decoded;
-//         next();
-//     })
-            
-// }
+
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
@@ -64,15 +49,7 @@ async function run(){
 
 
 
-        //Auth
-        // app.post('/login', async(req, res) => {
-        //     const user = req.body;
-        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        //         expiresIn: '1d'
-        //     });
-        //     res.send({ accessToken })
-
-        // })
+        
 
 
         //get service api
@@ -141,7 +118,6 @@ async function run(){
         //get Order api
 
         app.get('/order', verifyJWT, async(req, res) => {
-            
             const email = req.query.email;
             const query = {email: email};
             const cursor = orderCollection.find(query);
@@ -149,20 +125,7 @@ async function run(){
             res.send(orders);
         })
 
-        // // get order api 
-        // app.get('/order', verifyJWT,  async(req, res) => {
-        //     const decodedEmail = req.decoded.email;
-        //     const email = req.query.email;
-        //     if(email === decodedEmail){
-        //         const query = {email};
-        //     const cursor = orderCollection.find(query);
-        //     const orders = await cursor.toArray();
-        //     return res.send(orders);
-        //     }
-        //     else{
-        //         return res.status(403).send({message: 'Forbidden access'})
-        //     }
-        // })
+        
 
         
         })
